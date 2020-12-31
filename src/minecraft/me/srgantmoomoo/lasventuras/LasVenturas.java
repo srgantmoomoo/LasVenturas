6c1b5dccdb2612f325f6a90b2148fcc34f807ea3
joey <goldenrighty@gmail.com>
2020-12-31 03:48:51 -0500

    :D

---
+++ src/minecraft/me/srgantmoomoo/lasventuras/LasVenturas.java
@@ -0,0 +1,13 @@
+package me.srgantmoomoo.lasventuras;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class LasVenturas {
+		
+		public static String NAME = "LasVenturas", VERSION = "1";
+		
+		public static void init() {
+
+	}
+
+}
